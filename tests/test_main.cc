bbae156e5481676c3dcfbd4ece242943fcfddd64
Minh Phan <69239650+phantuanminh@users.noreply.github.com>
2021-04-17 20:14:37 +0700

    Revert "Basic Project Structure Setup"
    This reverts commit 1abb575fef699115bbe7334e5d771c2d4e416ce0.

--- tests/test_main.cc
+++
@@ -1,5 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
-// This file just creates an entry-point (i.e. defines a main function)
-// for your test executable; you don't need to modify it.
