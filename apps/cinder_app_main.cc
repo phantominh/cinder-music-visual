5c9a3a5a13947c7346e64a13175de348d199ba27
Minh Phan <69239650+phantuanminh@users.noreply.github.com>
2021-04-17 14:20:38 +0700

    App: Load and Play Audio Files

---
+++ apps/cinder_app_main.cc
@@ -0,0 +1,7 @@
+#include "music_visual_app.h"
+
+using musicvisual::MusicVisualApp;
+
+// This line is a macro that expands into an "int main()" function.
+CINDER_APP(MusicVisualApp, ci::app::RendererGl);
+
