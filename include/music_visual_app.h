be93fd6883944df757a13c64a5248a83571eeada
Minh Phan <69239650+phantuanminh@users.noreply.github.com>
2021-04-17 19:21:45 +0700

    Input audio with Buffer Player Node. TODO: Handle and Visualize the Audio

--- include/music_visual_app.h
+++ include/music_visual_app.h
@@ -3,7 +3,9 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
-#include <cinder/audio/Voice.h>
+
+#include "cinder/audio/audio.h"
+#include "cinder/audio/Voice.h"
 
 namespace musicvisual {
 
@@ -20,8 +22,13 @@ class MusicVisualApp : public App {
 
   void update() override;
 
+  void keyDown( KeyEvent event ) override;
+
+  void mouseDown( MouseEvent event ) override;
+
  private:
-  audio::VoiceRef mVoice;
+  // Node for sample audio playback
+  audio::BufferPlayerNodeRef buffer_player_node_;
 };
 
 }  // namespace musicvisual
--- src/music_visual_app.cc
+++ src/music_visual_app.cc
@@ -6,14 +6,28 @@ MusicVisualApp::MusicVisualApp() {
 }
 
 void MusicVisualApp::setup() {
-  audio::SourceFileRef sourceFile = audio::load( app::loadAsset( "01 Ballade No. 1 in G Minor, Op. 23.m4a" ) );
-  mVoice = audio::Voice::create( sourceFile );
+  auto ctx = audio::Context::master();
 
-  // Start playing audio from the voice:
-  mVoice->start();
+  // Create a SourceFile and set its output sample rate to match the context
+  audio::SourceFileRef source_file =
+      audio::load(app::loadAsset("01 Ballade No. 1 in G Minor, Op. 23.m4a"),
+                  ctx->getSampleRate());
+
+  // Initialize the buffer player node
+  buffer_player_node_ = ctx->makeNode(new audio::BufferPlayerNode());
+  buffer_player_node_->loadBuffer(source_file);
+
+  // Connect nodes and context
+  buffer_player_node_ >> ctx->getOutput();
+
+  buffer_player_node_->enable();
+  ctx->enable();
 }
 
 void MusicVisualApp::draw() {
+  gl::clear();
+  gl::enableAlphaBlending();
+
   AppBase::draw();
 }
 
@@ -21,4 +35,17 @@ void MusicVisualApp::update() {
   AppBase::update();
 }
 
+void MusicVisualApp::keyDown(KeyEvent event) {
+  if (event.getCode() == KeyEvent::KEY_SPACE) {
+    if (buffer_player_node_->isEnabled())
+      buffer_player_node_->stop();
+    else
+      buffer_player_node_->start();
+  }
 }
+
+void MusicVisualApp::mouseDown(MouseEvent event) {
+  buffer_player_node_->start();
+}
+
+}  // namespace musicvisual
